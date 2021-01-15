65255ec4cfca6c25c1b4d40cce9e502948c351ba
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-15 08:11:50 -0300

    chnaged Vue.js v2 to Vue.js v3

--- docs/index.js
+++ docs/index.js
@@ -1,17 +1,24 @@
-var app = new Vue({
-	el: "main",
-	data: {
-		toDos: [],
-		newTodo: null
+const app = Vue.createApp({
+	data() {
+		return {
+			toDos: [],
+			newTodo: null
+		};
 	},
 	mounted() {
 		this.toDos = localStorage.toDos ? JSON.parse(localStorage.toDos) : [];
 	},
 	methods: {
 		finishe(toDo) {
-			const index = this.toDos.indexOf(toDo);
-			this.toDos[index] = toDo;
-			localStorage.toDos = JSON.stringify(this.toDos);
+			toDo.finished = !toDo.finished;
+
+			localStorage.toDos = JSON.stringify(this.toDos.map((element) => {
+				return {
+					description: element.description,
+					finished: element.finished,
+					edit: element.edit
+				};
+			}));
 		},
 		store(e, description) {
 			if (e.which == 13) {
@@ -20,19 +27,31 @@ var app = new Vue({
 					finished: false,
 					edit: false
 				});
-				localStorage.toDos = JSON.stringify(this.toDos);
+
+				localStorage.toDos = JSON.stringify(this.toDos.map((element) => {
+					return {
+						description: element.description,
+						finished: element.finished,
+						edit: element.edit
+					};
+				}));
 				this.newTodo = null;
 			}
 		},
 		edit(toDo) {
-			toDo.edit = true
+			toDo.edit = true;
 		},
 		update(e, toDo) {
 			if (e.which == 13) {
-				const index = this.toDos.indexOf(toDo);
-				toDo.edit = false;
-				this.toDos[index] = toDo;
-				localStorage.toDos = JSON.stringify(this.toDos);
+				this.cancelUpdate(toDo);
+
+				localStorage.toDos = JSON.stringify(this.toDos.map((element) => {
+					return {
+						description: element.description,
+						finished: element.finished,
+						edit: element.edit
+					};
+				}));
 			}
 		},
 		cancelUpdate(toDo) {
@@ -41,7 +60,20 @@ var app = new Vue({
 		remove(toDo) {
 			const index = this.toDos.indexOf(toDo);
 			this.toDos.splice(index, 1);
-			localStorage.toDos = JSON.stringify(this.toDos);
+			localStorage.toDos = JSON.stringify(this.toDos.map((element) => {
+				return {
+					description: element.description,
+					finished: element.finished,
+					edit: element.edit
+				};
+			}));
+		}
+	},
+	directives: {
+		focus: {
+			mounted(element) {
+				element.focus();
+			}
 		}
 	}
-})
+}).mount('main');
